29d7da97e5160d9a92b780252103c9b6f840e22c
yohira <39988933+yohira-repo@users.noreply.github.com>
2022-08-18 14:25:18 +0900

    Section14-OK

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -14,11 +14,27 @@ export const Emotion = () => {
     justify-content: space-around;
     align-items: center;
   `;
+  const titleStyle = css({
+    margin: 0,
+    color: "#3d84a8"
+  });
 
   return (
     <div css={containerStyle}>
-      <p>- Emotion -</p>
-      <button>FIGHT!!</button>
+      <p css={titleStyle}>- Emotion -</p>
+      <SButton>FIGHT!!</SButton>
     </div>
   );
 };
+
+const SButton = styled.button`
+  background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
